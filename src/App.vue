06b4b243db2f9a758e30191fc162d7521fd9d1ca
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2020-03-14 19:25:47 +0700

    setting vue

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,21 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 #app {
